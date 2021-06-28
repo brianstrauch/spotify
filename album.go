c709e7a96bb3bf0fefd3e3816b1b5bbe472a5e04
Ted Wexler <ted@stuckinacan.com>
2021-06-28 18:23:45 -0400

    Remove album type aliases and consts

--- album.go
+++ album.go
@@ -6,29 +6,14 @@ import "time"
 // https://developer.spotify.com/documentation/web-api/reference/#object-albumobject
 type Album struct {
 	Meta
-	AlbumType            AlbumType `json:"album_type"`
+	AlbumType            string    `json:"album_type"`
 	AvailableMarkets     []string  `json:"available_markets"`
 	Images               []Image   `json:"images"`
 	Label                string    `json:"label"`
 	Popularity           int       `json:"popularity"`
 	ReleaseDate          time.Time `json:"release_date"`
-	ReleaseDatePrecision Precision `json:"release_date_precision"`
+	ReleaseDatePrecision string    `json:"release_date_precision"`
 	TotalTracks          int       `json:"total_tracks"`
 	Tracks               Tracks    `json:"tracks"`
 	Name                 string    `json:"name"`
 }
-
-type Precision string
-
-const (
-	Day   Precision = "day"
-	Month Precision = "month"
-	Year  Precision = "year"
-)
-
-type AlbumType string
-
-const (
-	TypeSingle AlbumType = "single"
-	TypeAlbum  AlbumType = "album"
-)
