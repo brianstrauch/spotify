f016396f774f93b074773410aef703c0ca2392ec
Threpio <Theomorris@hotmail.co.uk>
2021-07-21 15:44:46 +0200

    Sorted out paging as per suggestions

--- paging.go
+++ paging.go
@@ -3,6 +3,7 @@ package spotify
 // Paging represents a PagingObject in the Spotify API
 // https://developer.spotify.com/documentation/web-api/reference/#object-pagingobject
 type Paging struct {
+	Albums AlbumPage `json:"albums"`
 	Tracks TrackPage `json:"tracks"`
 }
 
@@ -19,11 +20,6 @@ type PlaylistPage struct {
 type PlaylistTrackPage struct {
 	PagingMeta
 	Items []*PlaylistTrack `json:"items"`
-}
-
-// PagingAlbums represents an PagingObject in the spotify API for albums
-type PagingAlbums struct {
-	Albums AlbumPage `json:"albums"`
 }
 
 type AlbumPage struct {
