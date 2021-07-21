bb1c01a8b3e6610a2a650efb7730e0882b66b54b
Threpio <Theomorris@hotmail.co.uk>
2021-07-21 15:46:31 +0200

    Deleted arbitary Album functions

--- album.go
+++ album.go
@@ -1,10 +1,5 @@
 package spotify
 
-import (
-	"net/url"
-	"strconv"
-)
-
 // Album represents an AlbumObject in the Spotify API
 // https://developer.spotify.com/documentation/web-api/reference/#object-albumobject
 type Album struct {
@@ -19,29 +14,4 @@ type Album struct {
 	TotalTracks          int       `json:"total_tracks"`
 	Tracks               TrackPage `json:"tracks"`
 	Name                 string    `json:"name"`
-}
-
-// SearchAlbum returns a AlbumPaging object that includes a number of albums as per the limit set
-func (a *API) SearchAlbum(q, searchType string, limit int) (*PagingAlbums, error) {
-	v := url.Values{}
-	v.Add("q", q)
-	v.Add("type", searchType)
-	v.Add("limit", strconv.Itoa(limit))
-
-	pagingAlbums := new(PagingAlbums)
-	err := a.get("v1", "/search", v, pagingAlbums)
-
-	return pagingAlbums, err
-}
-
-func (a *API) GetAlbum(name string) (*Album, error) {
-
-	pagingAlbums, err := a.SearchAlbum(name, "album", 1)
-	if err != nil {
-		return nil, err
-	}
-
-	album := pagingAlbums.Albums.Items
-
-	return album[0], nil
 }
